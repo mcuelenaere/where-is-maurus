b78ee47d81f3760a9cf98aad4da677ece42dfa42
Maurus Cuelenaere <mcuelenaere@gmail.com>
2025-08-31 16:52:33 +0200

    chore(frontend): kill dead code

--- frontend/src/shared/utils/format.ts
+++ frontend/src/shared/utils/format.ts
@@ -1,21 +1,5 @@
-export function formatSpeedKph(v?: number) {
-  return v != null ? v.toFixed(0) : undefined;
-}
-
-export function formatHeading(v?: number) {
-  return v != null ? v.toFixed(0) : undefined;
-}
-
 export function formatKilometers(v?: number) {
   return v != null ? v.toFixed(2) : undefined;
-}
-
-export function formatPercent(v?: number) {
-  return v != null ? v.toFixed(0) : undefined;
-}
-
-export function formatPower(v?: number) {
-  return v != null ? v.toFixed(0) : undefined;
 }
 
 export function formatCelsius(v?: number) {
